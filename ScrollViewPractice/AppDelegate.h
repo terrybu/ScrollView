6e7c403adb69bcf7e9f308d06eaad39c7f746d07
Terry Bu <burea1124@gmail.com>
2015-03-19 11:33:32 -0400

    Initial Commit

---
+++ ScrollViewPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewPractice
+//
+//  Created by Terry Bu on 3/19/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewPractice
+//
+//  Created by Terry Bu on 3/19/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
